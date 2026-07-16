79fb3ffc3e82141a556161886f5b815e4fc6974f
Gianni Ballerini <Gianniballerini@gmail.com>
2026-07-16 19:50:21 -0300

    Add maplibre-gl

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,1 +1,9 @@
-console.warn('Start of the application');
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/globe.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+});
